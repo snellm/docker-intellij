5d33bd5cc82be4e429d94eb27d0d5dae909f9e12
Michael Snell <michael@snell.com>
2015-03-23 16:40:05 +0000

    Automate Docker file highlighting

--- idea.sh
+++ idea.sh
@@ -4,7 +4,7 @@
 # Convinience start script for IntelliJ IDEA
 #
 
-# Install syntax highligting for Docker files if not present
+# Install syntax highlighting for Docker files if not present
 if [ ! -f ~/.IdeaIC14/config/filetypes/Dockerfile.xml ]; then
     wget -O -q ~/.IdeaIC14/config/filetypes/Dockerfile.xml https://raw.githubusercontent.com/masgari/docker-intellij-idea/master/Dockerfile.xml
 fi
